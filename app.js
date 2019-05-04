a6a52cb02bc386fb4cab54a195a9954a604ace16
Robert <rplaird@gmail.com>
2019-05-04 15:54:11 -0400

    adding chunksize

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const argsHelper = require('./helpers/args');
 const { Worker } = require("worker_threads");
 const path = require("path");
 
-args = argsHelper.getArgs()
-
-if (!args.connections) args.connections = 4
-
-
-console.log('fetching  %s -  with %i connections', args.url, args.connections);
-
 const fetchWorkerScript = path.join(__dirname, "./workers/fetch_worker.js");
 const appendWorkerScript = path.join(__dirname, "./workers/append_worker.js");
+
+args = argsHelper.getArgs()
+
+if (!args.connections) args.connections = 4;
+if (!args.chunksize) args.chunksize = 50000;
+
+if (!args.url) {
+    console.error('--url not specified');
+    process.exit(1);
+ }
+
+console.log('fetching  %s in %i sized chunks with %i connections', args.url, args.chunksize, args.connections);
 
 var content = {
     connections: args.connections,
     url: args.url,
-    chunksize: 50000
+    chunksize: args.chunksize
 }
 
 var waitingForConnectionsToFinish = args.connections;
@@ -28,7 +33,7 @@ for (var i = 0; i < args.connections; i++) {
 
 function onResult() {
     waitingForConnectionsToFinish--;
-    if (!waitingForConnectionsToFinish) {
+    if (waitingForConnectionsToFinish <= 0) {
         const worker = new Worker(appendWorkerScript, { workerData: args.connections });
         worker.on("exit", () => console.log('done'));
     }
--- helpers/args.js
+++ helpers/args.js
@@ -4,8 +4,9 @@ module.exports = {
     getArgs: function () {
         args
             .version('0.1.0')
-            .option('-c, --connections [n]', 'number of connections', parseInt)
             .option('-u, --url [url]', 'file url')
+            .option('-c, --connections [n]', 'number of connections', parseInt)
+            .option('-C, -chunksize [n]', 'size in bytes', parseInt)
             .parse(process.argv);
         return args
     }
